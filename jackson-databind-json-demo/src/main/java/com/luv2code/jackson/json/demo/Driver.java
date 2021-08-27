b02a8a41ce5197398eff4fede087b72f4a9b9940
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-27 11:07:37 +0530

    Spring REST: JSON Jackson - Nested Objects and Arrays, Ignore Unknown Properties

--- jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -11,16 +11,35 @@ public class Driver {
 
 		try {
 			
-			// craete object mapper
+			// create object mapper
 			ObjectMapper mapper = new ObjectMapper();
 			
 			// read JSON file (data/sample-lite.json) and map/convert to Java POJO.
 			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
 			// Above, we are reading from sample-lite.json file which is in data directory.
 			
+			// read JSON file (data/sample-full.json) and map/convert to Java POJO.
+			Student theStudent2 = mapper.readValue(new File("data/sample-full.json"), Student.class);
+			// We have to also create a new Address class for Address Nested field in this
+			// sample-full.json file. (See Address class).
+			// AND then we need to Add this newly created Address type field into our Student class 
+			// and also appropriate getter/setter method for it.
+			// AND the Finally, to store languages field from json file, we need to add String array
+			// in our Student class because in json file, we have languages stored in an array.
+			
 			// print first name and last name
 			System.out.println("First name: " + theStudent.getFirstName());
 			System.out.println("Last name: " + theStudent.getLastName());
+			
+			// print out address: street and city
+			Address tempAddress = theStudent2.getAddress();
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
+			
+			// print out languages
+			for (String tempLang : theStudent2.getLanguages()) {
+				System.out.println(tempLang);
+			}
 			
 		} catch (Exception exc) {
 			exc.printStackTrace();
--- jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -1,11 +1,24 @@
 package com.luv2code.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+///// @JsonIgnoreProperties ANNOTATION /////
+// This will INGORE PROPERTIES from JSON file for which we have NOT defined any fields 
+// in this class.
+// If we did not do this, then it will cause an Exception due to Unknown Properties.
+// So now, it will Ignore Unknown Properties.
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Student {
 
 	private int id;
 	private String firstName;
 	private String lastName;
 	private boolean active;
+	
+	private Address address; // Adding Address field to store address from sample-full.json file.
+	
+	private String[] languages; //  Adding languages field to store languages array from json file.
 	
 	public Student() {
 		
@@ -41,6 +54,22 @@ public class Student {
 
 	public void setActive(boolean active) {
 		this.active = active;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String[] getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String[] languages) {
+		this.languages = languages;
 	}
 	
 	
